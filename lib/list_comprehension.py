8ba74d6cc6aa4880111b4a8730ac6204310c9b2e
LYDIA <lydiakinuthia91@gmail.com>
2025-05-17 11:04:15 +0300

    initial commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python3
-
 def return_evens(num_list):
+    evens = [n for n in num_list if n % 2 == 0]
+    return evens
     pass
 
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
 def make_exclamation(sentence_list):
+    pass
+    exclamation_list = [s + "!" for s in sentence_list]
+    return exclamation_list
     pass
